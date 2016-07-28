215a2075293e9c67a5ab495a0d283e1308ac479e
Richie Yi <helloworldrichie@gmail.com>
2016-07-27 22:59:47 -0700

    Create Board class and add .gitignore

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,31 @@ class Board
     @grid = ('1'..size.to_s).to_a
   end
 
+  def available_moves
+    grid.reject { |piece| piece == 'X' || piece == 'O' }
+  end
+
+  def get_piece_at(location)
+    grid[location - 1]
+  end
+
+  def set_piece_at(location, piece)
+    grid[location - 1] = piece
+  end
+
+  def reset_piece_at(location)
+    grid[location - 1] = location.to_s
+  end
+
+  def rows
+    grid.each_slice(Math.sqrt(size)).to_a
+  end
+
+  def columns
+    rows.transpose
+  end
+
+  def diagonals
+    [[grid[0], grid[4], grid[8]], [grid[2], grid[4], grid[6]]]
+  end
 end
